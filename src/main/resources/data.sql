075e9c00ec716a7e7d430f804fcbb03798052ed6
Amal Thomas <amal.thomas@nissanmotor.com>
2020-04-27 16:52:47 +0530

    SQL Queries, Update.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,12 +10,7 @@ CREATE TABLE patterns (
  
 INSERT INTO patterns (type, name, pattern, sample)
 	values
-	('MovieFileName', 'TamilRockers', 'www.TamilRockers.[a-z]{2,4} - (?<title>[a-zA-Z ()-]*) \((?<year>[0-9]{4})\) *\[[a-zA-Z 0-9\-]*\].[a-z0-9]{2,4}','');
-INSERT INTO patterns (type, name, pattern, sample)
-	values
-	('MovieFileName', 'TamilRockerrs', 'www.TamilRockerrs.[a-z]{2,4} - (?<title>[a-zA-Z ()-]*) \((?<year>[0-9]{4})\) *\[[a-zA-Z 0-9\-]*\].[a-z0-9]{2,4}','www.TamilRockerrs.co - Lonappante Mamodisa (2019) [Malayalam Original - DVDRip - x264 - 850MB].mkv');
-INSERT INTO patterns (type, name, pattern, sample)
-	values
-	('MovieFileName','Normal','(?<title>[a-zA-Z \)\(]+) *\[(?<year>[0-9]{4})\].[a-z0-9]{2,4}','');
-INSERT INTO patterns (type, name, pattern, sample)
-  	values('MovieFileName','Normal_2','(?<title>[a-zA-Z \)\(]+) *\((?<year>[0-9]{4})\).[a-z0-9]{2,4}','');
+	('MovieFileName', 'TamilRockers', 'www.TamilRockers.[a-z]{2,4} - (?<title>[a-zA-Z ()-]*) \((?<year>[0-9]{4})\) *\[[a-zA-Z 0-9\-]*\].[a-z0-9]{2,4}',''),
+	('MovieFileName', 'TamilRockerrs', 'www.TamilRockerrs.[a-z]{2,4} - (?<title>[a-zA-Z ()-]*) \((?<year>[0-9]{4})\) *\[[a-zA-Z 0-9\-]*\].[a-z0-9]{2,4}','www.TamilRockerrs.co - Lonappante Mamodisa (2019) [Malayalam Original - DVDRip - x264 - 850MB].mkv'),
+	('MovieFileName','Normal','(?<title>[a-zA-Z \)\(]+) *\[(?<year>[0-9]{4})\].[a-z0-9]{2,4}',''),
+  ('MovieFileName','Normal_2','(?<title>[a-zA-Z \)\(]+) *\((?<year>[0-9]{4})\).[a-z0-9]{2,4}','');
