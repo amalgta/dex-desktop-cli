4c59de9c63458177ea512db0b3fe973374f13ab5
Amal Thomas <amal.thomas@nissanmotor.com>
2020-04-30 14:21:57 +0530

    SQL Autocreate hotfix.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,13 +1,3 @@
-DROP TABLE IF EXISTS patterns;
- 
-CREATE TABLE patterns (
-  id INT AUTO_INCREMENT  PRIMARY KEY,
-  type VARCHAR(250) NOT NULL,
-  name VARCHAR(250) NOT NULL,
-  pattern VARCHAR(250) DEFAULT NULL,
-  sample VARCHAR(250) DEFAULT NULL
-);
- 
 INSERT INTO patterns (type, name, pattern, sample)
 	values
 	('MovieFileName', 'TamilRockers', 'www.TamilRockers.[a-z]{2,4} - (?<title>[a-zA-Z ()-]*) \((?<year>[0-9]{4})\) *\[[a-zA-Z 0-9\-]*\].[a-z0-9]{2,4}',''),
